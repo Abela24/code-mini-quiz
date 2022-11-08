f4d0c2d9a081e0f550d598712c2308c48ad9c74b
Abela24 <abelalemu95@gmail.com>
2022-11-08 17:31:42 -0500

    added code to render question

--- Assests/index.js
+++ Assests/index.js
@@ -1,5 +1,7 @@
+ //creating start button called start
  var Startbtn=document.querySelector(".start")
  console.log (Startbtn)
+ //60 seconds timer 
  var time = 60;
  var remainingtime = "";
  var questionEL = document.getElementById("questiondiv");
@@ -8,38 +10,42 @@
  var buttonc = document.getElementById("answer-c");
  var buttond = document.getElementById("answer-d");
  var timerEL = document.getElementById("Timer");
+ //click calls to start and start quiz
 Startbtn.addEventListener("click",function(){
     console.log ("click");
-    starttime();
+    startquiz();
 
 });
 
-
+//quiz questions
 var questions = [
-    {
-question1: "What does HTML stand for?",
-choices :[
-    "(a) HYper Trainer Marking Language",
-    "(b) Hyper Text Marketing Language",
-    "(c)Hyper Text MArkup Language",
-    "(d)Hyper Text Markup Leveler",
-],
-Answer: "(c)Hyper Text MArkup Language"
- },
+ 
  {
     question: "How many data type are there?",
+    //multiple choice options
     choices: [
         "(a)11",
         "(b)8",
         "(c)4",
         "(d)6",
     ],
-
+//correct answer
     Answer:"(b)8"
-}
+},
+{
+    question: "What does HTML stand for?",
+    choices : [
+        "(a) HYper Trainer Marking Language",
+        "(b) Hyper Text Marketing Language",
+        "(c)Hyper Text MArkup Language",
+        "(d)Hyper Text Markup Leveler",
+    ],
+    Answer: "(c)Hyper Text MArkup Language"
+     }
 
 ]
-var questionindex = 0;
+//insital start question on 0
+var questionindex = -1;
 var timeid;
 function startquiz (){
     remainingtime = time;
@@ -47,17 +53,25 @@ timeid = setInterval(starttime, 1000);
 timerEL.textContent = time;
 questionindex++
 startquestions();
-}
+};
 var starttime = function(){
 time--;
 timerEL.textContent = time;
 if(
    time <= 0
 ){
+    //when timer ennds , function calls to end quiz 
  endquiz();
 }
 }
 function startquestions(){
+    var currentQuestions = questions[questionindex];
+    //calling question
+    questionEL.textContent = currentQuestions.question;
+
+}
+
+function endquiz(){
 
 }
 
@@ -65,14 +79,13 @@ function startquestions(){
 
 var score=0;
 
-for(var i=o; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
-    if (response==qestions[i].prompt)
-    score++;
-    alert("Correct)
-}else
-alert("Wrong")
-{
+// for(var i=o; i < questions.length; i++){
+// var response = window.prompt(questions[i].prompt);
+// if (response==qestions[i].prompt)
+// score++;
+//  alert("Correct)
+// }else
+// alert("Wrong")
+// {
+// }
 
-    }
-}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # code-mini-quiz
-craeating a short mini code quiz
+craeating a short mini code quiz with timer that start from 60 seconds for every question.
+question is out of 4 and displays a mark out of 4 at the end
+
+
+when i click start,
+timer automatically starts with question in order as well with multiple choice answer
+
+
